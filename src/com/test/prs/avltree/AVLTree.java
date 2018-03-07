4da28af42324deb69d46853578a9334e2a8f2b29
Prashant Upadhya <prashant18691@gmail.com>
2018-03-07 22:45:14 +0530

    AVL Tree deletion

--- src/com/test/prs/avltree/AVLTree.java
+++ src/com/test/prs/avltree/AVLTree.java
@@ -13,41 +13,108 @@ public class AVLTree {
             return node;// duplicate node
         node.height=1+Math.max(height(node.left),height(node.right));
         int balanceFactor = getBalanceFactor(node);
-        if (balanceFactor>0 && data<node.left.data)// left left
+        if (balanceFactor>1 && data<node.left.data)// left left
             return rightRotate(node);
-        if (balanceFactor>0 && data>node.left.data) {// left right
+        if (balanceFactor>1 && data>node.left.data) {// left right
             node.left = leftRotate(node.left);
             return rightRotate(node);
         }
-        if (balanceFactor<0 && data>node.right.data)// right right
+        if (balanceFactor<-1 && data>node.right.data)// right right
             return leftRotate(node);
-        if (balanceFactor<0 && data<node.right.data){// right left
+        if (balanceFactor<-1 && data<node.right.data){// right left
             node.right=rightRotate(node.right);
             return leftRotate(node);
+        }
+        return node; }
+
+    public Node delete(Node node, int data){
+        if (node==null)
+            return null;
+        if (data<node.data)
+            node.left = delete(node.left,data);
+        else if (data>node.data)
+            node.right = delete(node.right,data);
+        else {//no child or one child
+            if (node.left == null || node.right == null) {
+                Node temp = null;
+                if (temp == node.left)
+                    temp = node.right;
+                else
+                    temp = node.left;
+
+                if (temp == null) {//no child
+                    temp = node;
+                    node = null;
+                } else
+                    node = temp;//one child
+            } else {
+                // both child
+                //get inorder successor
+                Node successor = getInorderSuccessor(node.right);
+                node.data = successor.data;
+                node.right = delete(node.right, successor.data);
+            }
+        }
+            if (node==null)
+                return null;
+
+            node.height = 1+Math.max(height(node.left), height(node.right));
+
+            int balanceFactor = getBalanceFactor(node);
+
+            if (balanceFactor>1 && getBalanceFactor(node.left)>=0)//left left
+                return rightRotate(node);
+            if (balanceFactor>1 && getBalanceFactor(node.left)<0) {//left right
+                node.left = leftRotate(node.left);
+                return rightRotate(node);
+            }
+            if (balanceFactor<-1 && getBalanceFactor(node.right)<=0)//right right
+                return leftRotate(node);
+            if (balanceFactor<-1 && getBalanceFactor(node.right)>0){//right left
+                node.right=rightRotate(node.right);
+                return leftRotate(node);
+            }
+
+        return node;
+    }
+
+    private Node getInorderSuccessor(Node node) {
+        while (node.left!=null){
+            node=node.left;
         }
         return node;
     }
 
-    private Node leftRotate(Node node) {
-//        System.out.print("left ");
-        Node y = node.right;
-        Node t1 = y.left;
-        node.right=t1;
-        y.left=node;
-        node.height=1+Math.max(height(node.left),height(node.right));
-        y.height=1+Math.max(height(y.left),height(y.right));
+    private Node leftRotate(Node x) {
+        Node y = x.right;
+        Node T2 = y.left;
+
+        // Perform rotation
+        y.left = x;
+        x.right = T2;
+
+        // Update heights
+        x.height =  Math.max(height(x.left), height(x.right)) + 1;
+        y.height =  Math.max(height(y.left), height(y.right)) + 1;
+
+        // Return new root
         return y;
     }
 
-    private Node rightRotate(Node node) {
-//        System.out.print("right ");
-        Node y = node.left;
-        Node t1 = y.right;
-        node.left=t1;
-        y.right=node;
-        node.height=1+Math.max(height(node.left),height(node.right));
-        y.height=1+Math.max(height(y.left),height(y.right));
-        return y;
+    private Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T2 = x.right;
+
+        // Perform rotation
+        x.right = y;
+        y.left = T2;
+
+        // Update heights
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+
+        // Return new root
+        return x;
     }
 
     private int getBalanceFactor(Node node) {
@@ -63,7 +130,7 @@ public class AVLTree {
 
     public static void main(String[] args) {
         AVLTree tree = new AVLTree();
-        tree.root=tree.insert(tree.root,10);
+        /*tree.root=tree.insert(tree.root,10);
         printPreorder(tree.root);
         System.out.println();
         tree.root=tree.insert(tree.root,20);
@@ -80,7 +147,39 @@ public class AVLTree {
         System.out.println();
         tree.root=tree.insert(tree.root,25);
         printPreorder(tree.root);
+        System.out.println();*/
+
+        tree.root = tree.insert(tree.root, 9);
+        printPreorder(tree.root);
         System.out.println();
+        tree.root = tree.insert(tree.root, 5);
+        printPreorder(tree.root);
+        System.out.println();
+        tree.root = tree.insert(tree.root, 10);
+        printPreorder(tree.root);
+        System.out.println();
+        tree.root = tree.insert(tree.root, 0);
+        printPreorder(tree.root);
+        System.out.println();
+        tree.root = tree.insert(tree.root, 6);
+        printPreorder(tree.root);
+        System.out.println();
+        tree.root = tree.insert(tree.root, 11);
+        printPreorder(tree.root);
+        System.out.println();
+        tree.root = tree.insert(tree.root, -1);
+        printPreorder(tree.root);
+        System.out.println();
+        tree.root = tree.insert(tree.root, 1);
+        printPreorder(tree.root);
+        System.out.println();
+        tree.root = tree.insert(tree.root, 2);
+        printPreorder(tree.root);
+        System.out.println();
+        System.out.println("*********************************");
+        tree.root = tree.delete(tree.root,10 );
+        printPreorder(tree.root);
+
     }
 
     private static void printPreorder(Node root) {
@@ -97,5 +196,6 @@ class Node{
 
     public Node(int data) {
         this.data = data;
+        this.height=1;
     }
 }
