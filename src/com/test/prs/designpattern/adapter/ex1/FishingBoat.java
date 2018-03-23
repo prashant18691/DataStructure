9dd8049426921f8c8effa772ce2a7320310c9c8c
prashant18691 <prashant18691@gmail.com>
2018-03-23 21:23:56 +0530

    Design pattern

---
+++ src/com/test/prs/designpattern/adapter/ex1/FishingBoat.java
@@ -0,0 +1,7 @@
+package com.test.prs.designpattern.adapter.ex1;
+
+public class FishingBoat {
+    public void sail(){
+        System.out.println("Sailing");
+    }
+}
---
+++ src/com/test/prs/designpattern/adapter/ex1/RowingBoat.java
@@ -0,0 +1,5 @@
+package com.test.prs.designpattern.adapter.ex1;
+
+public interface RowingBoat {
+    public void row();
+}
