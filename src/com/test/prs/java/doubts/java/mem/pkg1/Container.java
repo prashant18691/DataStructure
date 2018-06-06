278bbe697503785ef9932a0bef07efa69a0029b6
prashant18691 <prashant18691@gmail.com>
2018-06-06 20:50:00 +0530

    misc

---
+++ src/com/test/prs/java/doubts/java/mem/pkg1/Container.java
@@ -0,0 +1,15 @@
+package com.test.prs.java.doubts.java.mem.pkg1;
+
+
+public class Container {
+    private String initial = "A";
+
+    public String getInitial() {
+        return initial;
+    }
+
+    public void setInitial(String initial) {
+        this.initial = initial;
+    }
+}
+
