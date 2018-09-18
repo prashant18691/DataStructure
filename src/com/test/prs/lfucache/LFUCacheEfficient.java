8351c7645615c29219fc4e5254280aeac5676de3
prashant18691 <prashant18691@gmail.com>
2018-09-18 17:57:18 +0530

    LFU cache in O(1)

--- src/com/test/prs/lfucache/LFUCacheEfficient.java
+++ src/com/test/prs/lfucache/LFUCacheEfficient.java
@@ -1,15 +1,13 @@
 package com.test.prs.lfucache;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.TreeMap;
 
-
+// O(1) complexity
 public class LFUCacheEfficient {
-    private Map<Integer,Integer> valueMap = new HashMap<>();//holds key-value
-    private Map<Integer,Integer> countMap = new HashMap<>();// holds key-count
-    private TreeMap<Integer, LinkedList<Integer>> frequencyMap = new TreeMap<>();// holds count-LinkedList<key>
+    private Map<Integer,Node> valueMap = new HashMap<>();//holds key-value
+    private TreeMap<Integer, DoubleLinkedList> frequencyMap = new TreeMap<>();// holds count-LinkedList<key>
     private Integer MAX_CAPACITY;
 
     public LFUCacheEfficient(final Integer MAX_CAPACITY) {
@@ -19,28 +17,81 @@ public class LFUCacheEfficient {
     public Integer get(int key){
         if (!valueMap.containsKey(key))
             return -1;
-        int frequency = countMap.get(key);
-        countMap.put(key,frequency+1);
-        frequencyMap.get(frequency).remove(new Integer(key));
+        Node node = valueMap.get(key);
+        int frequency = node.count;
+        node.count++;
+        frequencyMap.get(frequency).remove(node);
         if (frequencyMap.get(frequency).size()==0)
             frequencyMap.remove(frequency);
-        frequencyMap.computeIfAbsent(frequency+1, k -> new LinkedList<>()).add(key);
-        return valueMap.get(key);
+        frequencyMap.computeIfAbsent(frequency+1, k -> new DoubleLinkedList()).add(node);
+        return node.value;
     }
 
     public void set(int key, int value){
         if (!valueMap.containsKey(key)){
+            Node node = new Node(key, value);
             if (valueMap.size()==MAX_CAPACITY){
                 int lowestCount = frequencyMap.firstKey();
-                int keyToBeEvicted = frequencyMap.get(lowestCount).removeFirst();
+                Node nodeToDelete = frequencyMap.get(lowestCount).getHead();
+                frequencyMap.get(lowestCount).remove(nodeToDelete);
                 if (frequencyMap.get(lowestCount).size()==0)
                     frequencyMap.remove(lowestCount);
-                countMap.remove(keyToBeEvicted);
-                valueMap.remove(keyToBeEvicted);
+//                countMap.remove(nodeToDelete.key);
+                valueMap.remove(nodeToDelete.key);
             }
-            valueMap.put(key,value);
-            countMap.put(key,1);
-            frequencyMap.computeIfAbsent(1, k -> new LinkedList<>()).add(key);
+            valueMap.put(key,node);
+            node.count++;
+//            countMap.put(key,1);
+            frequencyMap.computeIfAbsent(1, k -> new DoubleLinkedList()).add(node);
         }
+    }
+
+}
+
+class Node{
+    int key, value, count;
+    Node next,prev;
+
+    Node(final int key, final int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+class DoubleLinkedList{
+    int n;
+    Node head, tail;
+
+    void add(Node node){
+        if (head==null) {
+            head = node;
+            tail = node;
+            return;
+        }
+        tail.next = node;
+        node.prev = tail;
+        tail = node;
+        n++;
+    }
+
+    void remove(Node node){
+        if (node.next==null) { // tail
+            tail = node.prev;
+        }
+        else if(node.prev==null){ // head
+           head = node.next;
+        }
+        else{
+            node.prev.next = node.next;
+        }
+        n--;
+    }
+
+    Node getHead(){
+        return head;
+    }
+
+    int size(){
+        return n;
     }
 }
